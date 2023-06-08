149d3ce20490c8370adf2c43101f5e624858aab0
zune710 <126778780+zune710@users.noreply.github.com>
2023-06-08 13:51:04 +0900

    0608 Math

--- Project3D/Assets/Script/Math/CenterController.cs
+++ Project3D/Assets/Script/Math/CenterController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CenterController : MonoBehaviour
 {
+    /* // 동그라미
     public List<GameObject> PointList;
     
     void Start()
@@ -21,5 +22,29 @@ public class CenterController : MonoBehaviour
 
             PointList.Add(obj);
         }
+    }
+    */
+
+    // 점프
+    [Range(-90.0f, 90.0f)]
+    public float Angle;
+
+    private void Start()
+    {
+        gameObject.AddComponent<MyGizmo>();
+
+        //Angle = 5.0f;
+    }
+
+    private void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 Movement = new Vector3(hor, Mathf.Sin(Angle * Mathf.Deg2Rad) * 5.0f, ver) * 5.0f * Time.deltaTime;
+
+        transform.Translate(Movement);
+
+        transform.position = Movement;
     }
 }
